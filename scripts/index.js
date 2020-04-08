b74ac45fa26401db5b633efcebae5d077a6268d4
Wade <wsw0615@gmail.com>
2020-04-08 22:52:16 +0800

    feat: vue addEmployeeInput()

--- scripts/index.js
+++ scripts/index.js
@@ -57,53 +57,53 @@ let lastInput = employee.length - 1;
 
 // 點擊按鈕新增欄位
 const addEmployeebtn = document.querySelector('#addEmployee');
-addEmployeebtn.addEventListener('click', () => {
-    addEmployeeInput();
-});
+// addEmployeebtn.addEventListener('click', () => {
+//     addEmployeeInput();
+// });
 
-function addEmployeeInput(){
-    employee = document.querySelectorAll('#inputShiftTable tbody tr');
-    lastInput = employee.length - 1;
-    let isEmployeeCodeEmpty = employee[lastInput].cells[0].children[0].value != '' ? true : false ;
-    let isEmployeeJobTypeEmpty = employee[lastInput].cells[1].children[0].value != '' ? true : false ;
-    let isEmployeeRankEmpty = employee[lastInput].cells[2].children[0].value != '' ? true : false ;
-    if( isEmployeeCodeEmpty || isEmployeeJobTypeEmpty || isEmployeeRankEmpty ){
-        console.log('要加新的一列');
-        inputList.append(document.createElement("tr"));
-        let lastchild = inputList.lastElementChild;
-        lastchild.classList.add('employee');
-        lastchild.innerHTML = `
-        <th><input type="text" name="PTCode" class="employeeCode"></th>
-        <td>
-            <select class="form-control" name="jobType">
-                <option value="">請選擇職種</option>
-                <option value="FT">正職</option>
-                <option value="PT" selected>PT</option>                    
-            </select>
-        </td>
-        <td>
-            <select class="form-control" name="rank">
-                <option value="">請選擇能力等級</option>
-                <option value="jun">Junior</option>
-                <option value="sen">Senior</option>                    
-            </select>
-        </td>
-        <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
-        <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
-        <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
-        <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
-        <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
-        <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
-        <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
-        <td><button type="button" class="btn btn-danger delEmployee">Del</button></td>
-        `
-        employee = document.querySelectorAll('#inputShiftTable tbody tr');
-        lastInput = employee.length - 1;
-        delEmployee();
-    } else {
-        console.log('你還有空格喔')
-    }
-};
+// function addEmployeeInput(){
+//     employee = document.querySelectorAll('#inputShiftTable tbody tr');
+//     lastInput = employee.length - 1;
+//     let isEmployeeCodeEmpty = employee[lastInput].cells[0].children[0].value != '' ? true : false ;
+//     let isEmployeeJobTypeEmpty = employee[lastInput].cells[1].children[0].value != '' ? true : false ;
+//     let isEmployeeRankEmpty = employee[lastInput].cells[2].children[0].value != '' ? true : false ;
+//     if( isEmployeeCodeEmpty || isEmployeeJobTypeEmpty || isEmployeeRankEmpty ){
+//         console.log('要加新的一列');
+//         inputList.append(document.createElement("tr"));
+//         let lastchild = inputList.lastElementChild;
+//         lastchild.classList.add('employee');
+//         lastchild.innerHTML = `
+//         <th><input type="text" name="PTCode" class="employeeCode"></th>
+//         <td>
+//             <select class="form-control" name="jobType">
+//                 <option value="">請選擇職種</option>
+//                 <option value="FT">正職</option>
+//                 <option value="PT" selected>PT</option>                    
+//             </select>
+//         </td>
+//         <td>
+//             <select class="form-control" name="rank">
+//                 <option value="">請選擇能力等級</option>
+//                 <option value="jun">Junior</option>
+//                 <option value="sen">Senior</option>                    
+//             </select>
+//         </td>
+//         <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
+//         <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
+//         <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
+//         <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
+//         <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
+//         <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
+//         <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
+//         <td><button type="button" class="btn btn-danger delEmployee">Del</button></td>
+//         `
+//         employee = document.querySelectorAll('#inputShiftTable tbody tr');
+//         lastInput = employee.length - 1;
+//         delEmployee();
+//     } else {
+//         console.log('你還有空格喔')
+//     }
+// };
 
 // 自動監測最後一欄值的變化來新增欄位
 // autoAddEmployeeInput(inputList,lastInputCode);
--- scripts/vue.js
+++ scripts/vue.js
@@ -1,8 +1,50 @@
 new Vue({
     el:'#app',
     methods: {
-        aaa: function(){
-            console.log('u did it');
+        addEmployeeInput: function (){
+            let inputList = document.querySelector('#inputShiftTable tbody');
+            let employee = document.querySelectorAll('#inputShiftTable tbody tr');
+            let lastInput = employee.length - 1;
+            
+            let isEmployeeCodeEmpty = employee[lastInput].cells[0].children[0].value != '' ? true : false ;
+            let isEmployeeJobTypeEmpty = employee[lastInput].cells[1].children[0].value != '' ? true : false ;
+            let isEmployeeRankEmpty = employee[lastInput].cells[2].children[0].value != '' ? true : false ;
+            if( isEmployeeCodeEmpty || isEmployeeJobTypeEmpty || isEmployeeRankEmpty ){
+                console.log('要加新的一列');
+                inputList.append(document.createElement("tr"));
+                let lastchild = inputList.lastElementChild;
+                lastchild.classList.add('employee');
+                lastchild.innerHTML = `
+                <th><input type="text" name="PTCode" class="employeeCode"></th>
+                <td>
+                    <select class="form-control" name="jobType">
+                        <option value="">請選擇職種</option>
+                        <option value="FT">正職</option>
+                        <option value="PT" selected>PT</option>                    
+                    </select>
+                </td>
+                <td>
+                    <select class="form-control" name="rank">
+                        <option value="">請選擇能力等級</option>
+                        <option value="jun">Junior</option>
+                        <option value="sen">Senior</option>                    
+                    </select>
+                </td>
+                <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
+                <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
+                <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
+                <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
+                <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
+                <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
+                <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
+                <td><button type="button" class="btn btn-danger delEmployee">Del</button></td>
+                `
+                employee = document.querySelectorAll('#inputShiftTable tbody tr');
+                lastInput = employee.length - 1;
+                delEmployee();
+            } else {
+                console.log('你還有空格喔')
+            }
         }
     },
 })
