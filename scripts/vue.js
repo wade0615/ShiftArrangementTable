f4031fe2ebcc76821e9ee7ea43477da94fccaaa6
Wade <wsw0615@gmail.com>
2020-04-24 22:03:15 +0800

    feat: try to del PT_data in PT_datas

--- scripts/vue.js
+++ scripts/vue.js
@@ -109,7 +109,7 @@ Vue.component('input-shifttable-list', {
                         <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
                         <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
                         <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
-                        <td><button v-on:click='delEmployee()' type="button" class="btn btn-danger">Del</button></td>
+                        <td><button v-on:click='delEmployee(index)' type="button" class="btn btn-danger" >Del</button></td>
                     </tr>
                     
                 </tbody>
@@ -119,7 +119,6 @@ Vue.component('input-shifttable-list', {
     </section>`,
     methods: {
         addEmployeeInput() {
-            let inputList = document.querySelector('#inputShiftTable tbody');
             let employee = document.querySelectorAll('#inputShiftTable tbody tr');
             let lastInput = employee.length - 1;
             
@@ -128,39 +127,10 @@ Vue.component('input-shifttable-list', {
             let isEmployeeRankEmpty = employee[lastInput].cells[2].children[0].value != '' ? true : false ;
             if( isEmployeeCodeEmpty || isEmployeeJobTypeEmpty || isEmployeeRankEmpty ){
                 console.log('要加新的一列');
-                inputList.append(document.createElement("tr"));
-                let lastchild = inputList.lastElementChild;
-                lastchild.classList.add('employee');
-                lastchild.innerHTML = `
-                <th><input type="text" name="PTCode" class="decorationLine employeeCode"></th>
-                <td>
-                    <select class="jobType form-control" name="jobType">
-                        <option value="">職稱</option>
-                        <option value="FT">正職</option>
-                        <option value="PT" selected>PT</option>                    
-                    </select>
-                </td>
-                <td>
-                    <select class="rank form-control" name="rank">
-                        <option value="">能力階級</option>
-                        <option value="jun">Junior</option>
-                        <option value="sen">Senior</option>                    
-                    </select>
-                </td>
-                <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
-                <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
-                <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
-                <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
-                <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
-                <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
-                <td><input type="checkbox" name="daySchedule"><br><input type="checkbox" name="nightSchedule"></td>
-                <td><button v-on:click='delEmployee()' type="button" class="btn btn-danger">Del</button></td>
-                `
-                employee = document.querySelectorAll('#inputShiftTable tbody tr');
-                lastInput = employee.length - 1;
-                // delEmployee();
+                this.PT_datas.push({code:''});
             } else {
-                console.log('你還有空格喔')
+                console.log('你還有空格喔');
+                alert('你的最後一欄是空的喔！');
             }
         },
         makeFTresourceTable(){
@@ -180,9 +150,9 @@ Vue.component('input-shifttable-list', {
             console.log("employeeResourceForecast", employeeResourceForecast)
             this.employeeResourceForecast = employeeResourceForecast
         },
-        delEmployee() {
-            console.log('Vue');
-            // this.closest("tr").remove();
+        delEmployee: function(e) {
+            console.log('Del Vue',e);
+            this.PT_datas.splice(e, 1);
         },
     },
 });
